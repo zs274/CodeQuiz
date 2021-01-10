b3ac98c157b4d1848ef3603514efd539a903e9cf
Zarin Salim <zarin_96@hotmail.co.uk>
2021-01-10 13:28:12 +0000

    added ID tags and created questions variable

---
+++ script.js
@@ -0,0 +1,28 @@
+// QUESTIONS
+var quiz = [{
+    question: "Commonly used data types do not include:",
+    options: ["strings()", "booleans()", "alerts()", "numbers()"],
+    answer: "alerts()"
+},
+{
+    question: "The condition in an if / else statement is enclosed within ____",
+    options: ["quotes()", "curly brackets()", "parentheses()", "square brackets()"],
+    answer: "parentheses()"
+},
+{
+    question: "Arrays in JavaScript can be used to store ____",
+    options: ["numbers and strings()", "other arrays()", "booleans()", "all of the above()"],
+    answer: "all of the above()"
+},
+{
+    question: "What does DOM stand for?",
+    options: ["Document Object Model()", "Display Object Management()", "Digital Ordinance Model()", "Desktop Oriented Mode()"],
+    answer: "Document Object Model()"
+},
+{
+    question: "What is used to add styling to a web page?",
+    options: ["HTML()", "CSS()", "Python()", "JavaScript()"],
+    answer: 'CSS()'
+},
+]
+
